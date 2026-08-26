62868166320b1493d1b5faf098c1e1bdeb691b4c
Pradeep-build <impradeep4560@gmail.com>
2026-08-26 11:52:47 +0530

    Update API backend destination

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,14 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'production'
-          ? 'https://YOUR-BACKEND-NAME.onrender.com/api/:path*' // Replace this with your Render URL after deploying
-          : 'http://127.0.0.1:8000/api/:path*',
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "https://deta-music-api.onrender.com/api/:path*" // <--- Paste your Render URL here
+            : "http://127.0.0.1:8000/api/:path*",
       },
-    ]
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
